62824bc6818c8a43dcc2ffadd32fc979cdce6f8c
Adrian Oliveira <37006432+AdrianOliv@users.noreply.github.com>
2021-04-02 12:23:19 -0300

    Update Desafio42_TipoTriangulo_CursoPython.py

--- Desafios_Mundo2_Python_CursoEmVideo/Desafio42_TipoTriangulo_CursoPython.py
+++ Desafios_Mundo2_Python_CursoEmVideo/Desafio42_TipoTriangulo_CursoPython.py
@@ -3,13 +3,13 @@ a = int(input('Valor da primeira reta: '))
 b = int(input('Valor da segunda reta: '))
 c = int(input('Valor da terceira reta: '))
 if a<(b+c) and b<(a+c) and c<(a+b):
-    print('\033[31mO triangulo PODE SER FORMADO a partir destas retas!\033[m')
+    print('\033[34mO triangulo PODE SER FORMADO a partir destas retas!\033[m')
 else:
-    print('O triangulo NÃO PODE ser formado!')
+    print('\033[31mO triangulo NÃO PODE ser formado!')
 
 if a == b and b==c :
-    print('\nTRIANGULO EQUILÁTERO.')
+    print('TRIANGULO EQUILÁTERO.\n')
 elif a != b and b != c :
-    print('\nTRIANGULO ESCALENO.')
+    print('TRIANGULO ESCALENO.\n')
 else :
-    print('\nTRIANGULO ISÓSCELES.')
+    print('TRIANGULO ISÓSCELES.\n')
