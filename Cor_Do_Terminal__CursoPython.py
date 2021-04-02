57e426d6d1a39f321395c741dca0d9dbfb755756
Adrian Oliveira <37006432+AdrianOliv@users.noreply.github.com>
2021-04-02 01:32:35 -0300

    Delete Cor_Do_Terminal__CursoPython.py

--- Cor_Do_Terminal__CursoPython.py
+++
@@ -1,3 +0,0 @@
-nome = input('Nome Completo: ').title().split()
-print('Olá \033[31m{} {}\033[m, tudo bem?'.format(nome[0], nome[len(nome)-1]))
-print('Olá \033[4;34m{} {}\033[m, tudo bem?'.format(nome[0], nome[len(nome)-1]))
