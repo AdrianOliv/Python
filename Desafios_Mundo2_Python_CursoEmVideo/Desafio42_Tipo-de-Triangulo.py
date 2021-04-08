ae9f56e08ea59c8e8c4eb71aa1cfd94ecc91525b
Adrian Oliveira <37006432+AdrianOliv@users.noreply.github.com>
2021-04-07 22:17:16 -0300

    Atualizado Identação do Código
    O tipo de triangulo mostrava mesmo quando não era possível realizar um.

--- Desafios_Mundo2_Python_CursoEmVideo/Desafio42_Tipo-de-Triangulo.py
+++ Desafios_Mundo2_Python_CursoEmVideo/Desafio42_Tipo-de-Triangulo.py
@@ -4,12 +4,11 @@ b = int(input('Valor da segunda reta: '))
 c = int(input('Valor da terceira reta: '))
 if a<(b+c) and b<(a+c) and c<(a+b):
     print('\033[34mO triangulo PODE SER FORMADO a partir destas retas!\033[m')
+    if a == b and b==c :
+        print('TRIANGULO EQUILÁTERO.\n')
+    elif a != b and b != c :
+        print('TRIANGULO ESCALENO.\n')
+    else :
+        print('TRIANGULO ISÓSCELES.\n')
 else:
     print('\033[31mO triangulo NÃO PODE ser formado!')
-
-if a == b and b==c :
-    print('TRIANGULO EQUILÁTERO.\n')
-elif a != b and b != c :
-    print('TRIANGULO ESCALENO.\n')
-else :
-    print('TRIANGULO ISÓSCELES.\n')
