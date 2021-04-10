90117d0e7fda2c66b021f9248303026c6a8b1adc
Adrian Oliveira <37006432+AdrianOliv@users.noreply.github.com>
2021-04-09 23:41:03 -0300

    Criação de mais dois exercícios

---
+++ Desafios_Mundo2_Python_CursoEmVideo/Desafio46_Contagem_Regressiva.py
@@ -0,0 +1,6 @@
+# Contagem regressiva de fogos de artifício
+from time import sleep
+for i in range(10, 0, -1):
+    print(i)
+    sleep(1)
+print('FELIZ ANO NOVO!')
---
+++ Desafios_Mundo2_Python_CursoEmVideo/Desafio47_Mostrar_Numeros_Pares.py
@@ -0,0 +1,3 @@
+# Criar programa que mostre numeros pares de um intervalo
+for i in range(0, 50, 2):
+    print(i)
