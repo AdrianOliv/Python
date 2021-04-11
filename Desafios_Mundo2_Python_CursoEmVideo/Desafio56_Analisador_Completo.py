c23ccf04b5b6ac0eda73530830e3245ee5f8c9e7
Adrian Oliveira <37006432+AdrianOliv@users.noreply.github.com>
2021-04-11 20:14:34 -0300

    Criação do Desafio 56

---
+++ Desafios_Mundo2_Python_CursoEmVideo/Desafio56_Analisador_Completo.py
@@ -0,0 +1,20 @@
+# Programa que lê o nome, idade e sexo de 4 pessoas.
+# Dizer a média de idade de todos, nome do mais velho e
+# quantas mulheres tem menos de 20 anos
+ma_idade = 0
+s_idade = 0
+n_mulheres = 0
+for i in range(4):
+    nome = input('Qual o seu nome: ')
+    idade = int(input('Idade: '))
+    sexo = input('Sexo (fem/mas): ')
+    s_idade += idade
+    if idade > ma_idade:
+        ma_idade = idade
+        ma_velho = nome
+    if sexo == 'fem' and idade <= 20:
+        n_mulheres += 1
+
+print(f'\033[32mA média de idade de todos é {s_idade/4}!\033[m')
+print(f'\033[32mO mais velho do grupo é o(a) {ma_velho} com {ma_idade} anos!\033[m')
+print(f'\033[32mO número de mulheres jovens menor que 20 anos é {n_mulheres}!\033[m')
