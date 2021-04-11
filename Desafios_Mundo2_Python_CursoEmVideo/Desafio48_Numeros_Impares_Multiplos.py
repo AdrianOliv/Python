b49a56aa37182bc08f6fb40798de96e242afa942
Adrian Oliveira <37006432+AdrianOliv@users.noreply.github.com>
2021-04-11 20:36:41 -0300

    Update Range Desafio 48

--- Desafios_Mundo2_Python_CursoEmVideo/Desafio48_Numeros_Impares_Multiplos.py
+++ Desafios_Mundo2_Python_CursoEmVideo/Desafio48_Numeros_Impares_Multiplos.py
@@ -1,6 +1,6 @@
 # Criar um programa que some os impares multiplos de 3
 soma = 0
-for i in range(1, 500, 2):
+for i in range(1, 501, 2):
     if i % 3 == 0:
         soma += i
 print('A soma é {}'.format(soma))
