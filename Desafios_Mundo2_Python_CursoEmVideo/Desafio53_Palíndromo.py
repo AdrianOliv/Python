7c4afa2cf23d4691bc1b9532a9b1c30e20990db0
Adrian Oliveira <37006432+AdrianOliv@users.noreply.github.com>
2021-04-11 22:20:10 -0300

    Update Desafio53

--- Desafios_Mundo2_Python_CursoEmVideo/Desafio53_Palíndromo.py
+++ Desafios_Mundo2_Python_CursoEmVideo/Desafio53_Palíndromo.py
@@ -1,7 +1,7 @@
 # Programa detectar palíndromo.
 # adicionando o lower no fim da frase ele reconhecerá
 # mesmo se a frase estiver capitalizada.
-frase = input('Digite algo: ').lower()
+frase = input('Digite algo: ').lower().strip()
 frase_contra = frase[::-1]
 if frase_contra == frase:
     print('\033[32mÉ um palíndromo!\033[m')
