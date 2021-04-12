d3eba819bae80bfa92d75e52a93357b00fb6c79c
Adrian Oliveira <37006432+AdrianOliv@users.noreply.github.com>
2021-04-11 22:56:06 -0300

    Update Desafio 54

--- Desafios_Mundo2_Python_CursoEmVideo/Desafio54_Grupo_da_Maioridade.py
+++ Desafios_Mundo2_Python_CursoEmVideo/Desafio54_Grupo_da_Maioridade.py
@@ -1,9 +1,11 @@
 # Programa que identifica pessoas maiores de idade.
+from datetime import date
 from time import sleep
 s = 0
 for i in range(7):
-    idade = int(input(f'Pessoa {i+1}, digite a sua idade: '))
-    if idade >= 21 :
+    ano = int(input(f'Pessoa {i+1}, o ano do seu nascimento: '))
+    if (date.today().year - ano) >= 21 :
         s += 1
-print(f'\n{s} pessoas são maiores de idade!\n')
+print(f'\n{s} pessoas são maiores de idade!')
+print(f'{7-s} pessoas ainda são menores de idade\n')
 sleep(2)
