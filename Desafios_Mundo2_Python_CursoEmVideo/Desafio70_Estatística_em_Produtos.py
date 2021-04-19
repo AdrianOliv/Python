6abba7754f1644491afeffc25c417270597f85a9
Adrian Oliveira <37006432+AdrianOliv@users.noreply.github.com>
2021-04-19 13:06:57 -0300

    Update Desafio70_Estatística_em_Produtos.py

--- Desafios_Mundo2_Python_CursoEmVideo/Desafio70_Estatística_em_Produtos.py
+++ Desafios_Mundo2_Python_CursoEmVideo/Desafio70_Estatística_em_Produtos.py
@@ -1,3 +1,25 @@
 # Lêr nome e preço de vários produtos.
 # Qual total gasto na compra, Quantos custam mais de mil.
 # Qual o nome do prod mais barato?
+
+i = 'y'
+s = hund = p_cheap = 0
+cheap = str()
+while i == 'y':
+    prod = input('Nome prod: ')
+    price = float(input('Preço: '))
+    if price >= 1000:
+        hund += 1
+    if s == 0:
+        cheap = prod
+        p_cheap = price
+    elif price < p_cheap:
+        p_cheap = price
+        cheap = prod
+    s += price
+    i = input('Continue (y/n)...')
+
+print(f'O total gasto foi R${s}.')
+print(f'{cheap} foi o prod mais barato.')
+print(f'{hund} produtos acima de mil.')
+print('Programa Finalizado!')
