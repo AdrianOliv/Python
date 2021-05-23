3b8646a83bce92792e82930392d3c00315590990
Adrian Oliveira <37006432+AdrianOliv@users.noreply.github.com>
2021-05-23 16:21:42 -0300

    Update Desafio78_Maior_e_Menor_na_Lista.py

--- Desafios_Mundo3_Python_CursoEmVideo/Desafio78_Maior_e_Menor_na_Lista.py
+++ Desafios_Mundo3_Python_CursoEmVideo/Desafio78_Maior_e_Menor_na_Lista.py
@@ -1,5 +1,5 @@
 # Programa que lê 5 valores em lista.
-# Mostrar o maior e o menor.
+# Mostrar o maior e o menor e as suas posições.
 
 n = [int(input("Digite um número: ")),
      int(input("Digite outro número: ")),
@@ -8,5 +8,12 @@ n = [int(input("Digite um número: ")),
      int(input("Digite o último número: "))]
     
 print(f'\nOs números digitados foram = \033[32m{n}\033[m')
-print(f'O maior número foi \033[32m{max(n)}\033[m.')
-print(f'O menor número foi \033[32m{min(n)}\033[m.')
+print(f'O menor número foi o {min(n)}, nas posições: ', end=" ")
+for i, c in enumerate(n):
+    if c == min(n):
+        print(i, end='... ')
+
+print(f'\nO maior número foi o {max(n)}, nas posições: ', end=" ")
+for i, c in enumerate(n):
+    if c == max(n):
+        print(i, end='... ')
