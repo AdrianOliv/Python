f87a91875d57f4c68a5be73a128069e43e246e44
Adrian Oliveira <37006432+AdrianOliv@users.noreply.github.com>
2021-05-30 01:12:23 -0300

    Update Desafio82_Dividindo_listas.py

--- Desafios_Mundo3_Python_CursoEmVideo/Desafio82_Dividindo_listas.py
+++ Desafios_Mundo3_Python_CursoEmVideo/Desafio82_Dividindo_listas.py
@@ -6,23 +6,23 @@ imp = []
 l = []
 
 while True:
-    l.append(int(input("Digite um número: ")))
-	
-    while True:
-        op = input("Continuar(y/n)? ")
-	    if op == "y" or op == "n":
-	        break
-	    else:
-	        print("Valor inválido!")
-        if op == "n":
-            break
+	l.append(int(input("Digite um número: ")))
 
-    for i in l:
-        if i % 2 == 0:
-            p.append(i)
-        else:
-            imp.append(i)
+	while True:
+		op = input("Continuar(y/n)? ")
+		if op in 'yn':
+			break
+		else:
+			print("Valor Inválido!")
+	if op in "n":
+		break
 
-print(f'\nOs valores digitados foram: {l}')
-print(f'Os valores pares: {p}')
-print(f'E os ímpares: {imp}')
+for i in l:
+	if i % 2 == 0:
+		p.append(i)
+	else:
+		imp.append(i)
+
+print(f'\nVocê digitou os seguintes números {l}.')
+print(f'Os números pares foram {p}.')
+print(f'Os números ímpares são {imp}.')
