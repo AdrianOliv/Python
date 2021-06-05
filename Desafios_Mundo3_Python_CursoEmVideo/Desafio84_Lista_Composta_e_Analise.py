7c6f2684f4f21a356a9d8ed77b8dd0f23f723713
Adrian Oliveira <37006432+AdrianOliv@users.noreply.github.com>
2021-06-05 17:19:23 -0300

    Update Desafio84_Lista_Composta_e_Analise.py

--- Desafios_Mundo3_Python_CursoEmVideo/Desafio84_Lista_Composta_e_Analise.py
+++ Desafios_Mundo3_Python_CursoEmVideo/Desafio84_Lista_Composta_e_Analise.py
@@ -7,14 +7,14 @@ i = mai = men = 0
 while True:
     dado.append(str(input('Nome: ')))
     dado.append(int(input('Peso: ')))
-	
+    
     if men == 0:
-	men = dado[1]
+        men = dado[1]
     if dado[1] > mai:
-	mai = dado[1]
+        mai = dado[1]
     elif dado[1] < men:
-	men = dado[1]
-	
+        men = dado[1]
+    
     n.append(dado[:])
     dado.clear()
     i += 1
@@ -25,18 +25,19 @@ while True:
         else:
             print('Valor incorreto.')
     if op in 'Nn':
-	break
-
+        break
+    
 if len(n) == 1:
     print(f"\nFoi cadastrado somente uma pessoa e seu peso foi de {n[0][1]} kg.")
 else:
     print(f'\nForam cadastrados {i} pessoas.')
     print(f'O maior peso foi {mai} kg do(s) participante(s):')
     for p in n:
-	    if p[1] == mai:
-	        print(p[0])
-		
+        if p[1] == mai:
+            print(p[0])
+            
     print(f"O menor peso foi {men} kg do(s) participante(s):")
     for p in n:
         if p[1] == men:
-	        print(p[0])
+            print(p[0])
+            
