81aae8979c2a246150d1e8d39bf2fb45770b379d
Adrian Oliveira <37006432+AdrianOliv@users.noreply.github.com>
2021-06-05 09:09:22 -0300

    Update Desafio86_Matriz_em_Python.py

--- Desafios_Mundo3_Python_CursoEmVideo/Desafio86_Matriz_em_Python.py
+++ Desafios_Mundo3_Python_CursoEmVideo/Desafio86_Matriz_em_Python.py
@@ -4,11 +4,11 @@
 k = 1
 m = [[], [], []]
 for i in range(3):
-	for j in range(3):
-		m[i].append(int(input(f"Digite o {k} valor: ")))
-		k += 1
+    for j in range(3):
+	m[i].append(int(input(f"Digite o {k} valor: ")))
+	k += 1
 		
 for i in range(3):
-	print("\n")
-	for j in range(3):
-		print(f'|{m[i][j]:^5}|', end = "")
+    print("\n")
+    for j in range(3):
+	print(f'|{m[i][j]:^5}|', end = "")
