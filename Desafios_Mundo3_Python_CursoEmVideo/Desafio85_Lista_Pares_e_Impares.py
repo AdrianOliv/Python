27634903f54754d74a419cd0af8bd73709754762
Adrian Oliveira <37006432+AdrianOliv@users.noreply.github.com>
2021-06-05 07:56:59 -0300

    Create Desafio85_Lista_Pares_e_Impares.py

---
+++ Desafios_Mundo3_Python_CursoEmVideo/Desafio85_Lista_Pares_e_Impares.py
@@ -0,0 +1,15 @@
+# Programa para ler sete numeros.
+# Adicionar em uma lista unica, separando pares e impares.
+
+l = [[], []]
+for i in range(7):
+    num = int(input(f"Digite o {i+1} num: "))
+    if num % 2 == 0:
+    	if num not in l[0]:
+    		l[0].append(num)
+    else:
+    	if num not in l[1]:
+    		l[1].append(num)
+    	
+print(f'\nOs números pares foram : {sorted(l[0])}')
+print(f'Os números ímpares foram: {sorted(l[1])}')
