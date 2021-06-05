b23bdac0b542ed731b58dc3c34fb4a0f079ebc3d
Adrian Oliveira <37006432+AdrianOliv@users.noreply.github.com>
2021-06-05 08:35:54 -0300

    Create Desafio86_Matriz_em_Python.py

---
+++ Desafios_Mundo3_Python_CursoEmVideo/Desafio86_Matriz_em_Python.py
@@ -0,0 +1,14 @@
+# Programa que lê numeros e dispoe
+# em uma matriz.
+
+k = 1
+m = [[], [], []]
+for i in range(3):
+	for j in range(3):
+		m[i].append(int(input(f"Digite o {k} valor: ")))
+		k += 1
+		
+for i in range(3):
+	print("\n")
+	for j in range(3):
+		print(f'|{m[i][j]:^5}|', end = "")
