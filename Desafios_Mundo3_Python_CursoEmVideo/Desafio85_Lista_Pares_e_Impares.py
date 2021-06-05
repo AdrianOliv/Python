9b7141f2ad3724a8006858d12edd4299f148ee68
Adrian Oliveira <37006432+AdrianOliv@users.noreply.github.com>
2021-06-05 17:19:27 -0300

    Update Desafio85_Lista_Pares_e_Impares.py

--- Desafios_Mundo3_Python_CursoEmVideo/Desafio85_Lista_Pares_e_Impares.py
+++ Desafios_Mundo3_Python_CursoEmVideo/Desafio85_Lista_Pares_e_Impares.py
@@ -3,13 +3,13 @@
 
 l = [[], []]
 for i in range(7):
-    num = int(input(f"Digite o {i+1} num: "))
-    if num % 2 == 0:
-    	if num not in l[0]:
-    		l[0].append(num)
-    else:
-    	if num not in l[1]:
-    		l[1].append(num)
-    	
+	num = int(input(f"Digite o {i+1} num: "))
+	if num % 2 == 0:
+		if num not in l[0]:
+			l[0].append(num)
+		else:
+			if num not in l[1]:
+				l[1].append(num)
+
 print(f'\nOs números pares foram : {sorted(l[0])}')
 print(f'Os números ímpares foram: {sorted(l[1])}')
