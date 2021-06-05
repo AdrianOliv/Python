991e9959f9513594750b5eaff8e2502af1a4a055
Adrian Oliveira <37006432+AdrianOliv@users.noreply.github.com>
2021-06-05 09:06:36 -0300

    Update Desafio82_Dividindo_listas.py

--- Desafios_Mundo3_Python_CursoEmVideo/Desafio82_Dividindo_listas.py
+++ Desafios_Mundo3_Python_CursoEmVideo/Desafio82_Dividindo_listas.py
@@ -6,22 +6,22 @@ imp = []
 l = []
 
 while True:
-	l.append(int(input("Digite um número: ")))
+    l.append(int(input("Digite um número: ")))
 
-	while True:
-		op = input("Continuar(y/n)? ")
-		if op in 'yn':
-			break
-		else:
-			print("Valor Inválido!")
-	if op in "n":
-		break
+    while True:
+	op = input("Continuar(y/n)? ")
+	if op in 'YyNn':
+	    break
+	else:
+	    print("Valor Inválido!")
+	if op in "Nn":
+	    break
 
 for i in l:
-	if i % 2 == 0:
-		p.append(i)
-	else:
-		imp.append(i)
+    if i % 2 == 0:
+	p.append(i)
+    else:
+	imp.append(i)
 
 print(f'\nVocê digitou os seguintes números {l}.')
 print(f'Os números pares foram {p}.')
