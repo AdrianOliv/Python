64a0b26cacdf78e7c14fc563dd252a718c22e4ca
Adrian Oliveira <37006432+AdrianOliv@users.noreply.github.com>
2021-08-22 15:42:36 -0300

    Create Desafio101_Função_Voto.py

---
+++ Mundo3_Python_CursoEmVideo/Desafio101_Função_Voto.py
@@ -0,0 +1,15 @@
+# Begin
+from datetime import date
+
+def voto(ano):
+    if date.today().year - ano < 16:
+        return "NEGADO"
+    elif date.today().year - ano > 17:
+        return "OBRIGATÓRIO"
+    else:
+        return "OPCIONAL"
+
+
+# Main
+ano = int(input("Digite o seu ano de nascimento: "))
+print(f"Devido a sua idade, voto {voto(ano)}")
