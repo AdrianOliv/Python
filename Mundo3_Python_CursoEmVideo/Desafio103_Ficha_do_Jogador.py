8a3eca9926eeb304f2ecfedb056249fe8de1d930
Adrian Oliveira <37006432+AdrianOliv@users.noreply.github.com>
2021-08-22 21:57:20 -0300

    Create Desafio103_Ficha_do_Jogador.py

---
+++ Mundo3_Python_CursoEmVideo/Desafio103_Ficha_do_Jogador.py
@@ -0,0 +1,16 @@
+# Bagin
+def ficha(n="Desconhecido", g=0):
+    return f'O jogador {n} fez {g} gol(s) na partida.'
+
+# Main
+nome = str(input("Nome do jogador: "))
+gols = str(input("Gols marcados: "))
+if gols.isnumeric():
+    gols = int(gols)
+else:
+    gols = 0
+if nome.strip() == '':
+    print("ok")
+    print(ficha(g=gols))
+else:
+    print(ficha(nome, gols))
