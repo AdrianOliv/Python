eeb365129e54eaf6c95381e80ca48101344fb024
Adrian Oliveira <37006432+AdrianOliv@users.noreply.github.com>
2021-09-09 00:47:16 -0300

    Update Try_Except.py

--- Personal Projects/Try_Except.py
+++ Personal Projects/Try_Except.py
@@ -1,11 +1,12 @@
-# Usando Try e Except com factorial
-# A função try é excelente para evitar erros de programa que,
-# principalmente quando o usuário erro o tipo de entrada.
+# Usando Try e Except
+# A função try é excelente para evitar erros de programa,
+# principalmente quando o usuário erra o tipo de entrada.
 
-from math import factorial
 try:
-    x = int(input('Digite um número:  '))
-    print(factorial(x))
+    #Erro proposital, causado por uma string em uma função de inteiro.
+    print(int('Texto'))
 except:
-    # Se usuário digitar uma str ou float
-    print('Valor Inválido')
+    # Se aparecer algum problema
+    print('\nValor Inválido')
+finally:
+    print('Aparece no fim do programa, seja depois do try ou except!\n')
